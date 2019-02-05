588180d3be7aa6262ca40fa9ad162e317cdb53ea
PouRyakamalian <pooryak@gmail.com>
2019-02-05 11:58:30 +0330

    Simples

---
+++ video bar/scripts.js
@@ -0,0 +1,54 @@
+const player=document.querySelector('.player');
+const video=player.querySelector('.viewer');
+const progress=player.querySelector('.progress');
+const progressBar=player.querySelector('.progress__filled');
+const toggle=player.querySelector('.toggle');
+const skipButtons=player.querySelectorAll('[data-skip]');
+const ranges=player.querySelectorAll('.player__slider');
+const fullscreen=player.querySelector('.img');
+function togglePlay() {
+    if (video.paused){
+        video.play();
+    }  else  {
+        video.pause();
+    }
+}
+function updateButton (){
+    console.log("sd");
+const icon= this.paused ? "►" : "||";
+toggle.textContent=icon;
+}
+function skip () {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+function handeRangeUpdate(){
+   video[this.name]=this.value;
+   
+}
+function handleProgress (){
+const percent=(video.currentTime/video.duration)*100
+progressBar.style.flexBasis=`${percent}%`;
+}
+function scrub (e){
+    console.log(e);
+const scrubTime=(e.offsetX/progress.offsetWidth)*video.duration;
+console.log(scrubTime);
+video.currentTime=scrubTime;
+}
+function fullScreenDo (){
+    console.log("dfa");
+    video.requestFullscreen();
+}
+video.addEventListener('click',togglePlay);
+video.addEventListener('play',updateButton);
+video.addEventListener('timeupdate',handleProgress);
+video.addEventListener('pause',updateButton);
+toggle.addEventListener('click',togglePlay);
+skipButtons.forEach(button => button.addEventListener('click',skip));
+ranges.forEach(range => range.addEventListener('change',handeRangeUpdate));
+let mousedown=false;
+progress.addEventListener('click',scrub);
+progress.addEventListener('mousemove',(e)=>mousedown && scrub(e));
+progress.addEventListener('mousedown',()=>mousedown=true);
+(progress.addEventListener('mouseup',()=>mousedown=false));
+fullscreen.addEventListener('click',fullScreenDo);
